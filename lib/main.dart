73402c72e872747c913de5139507b43052e405ff
admin <6587RAJVIRANI>
2023-02-22 19:04:04 +0530

    22-02-2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:swipe_deck/swipe_deck.dart';
 import 'package:swipeable_card_stack/swipeable_card_stack.dart';
+
+const IMAGES = ["apex", "cod", "destiny", "fc3", "game_4", "ghost", "mk11", "nfs", "pubg", "mk112"];
+final borderRadius = BorderRadius.circular(20.0);
 
 void main() {
   runApp(MyApp());
@@ -9,14 +13,96 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Swipe Deck',
+      home: TestPage(),
     );
   }
 }
+
+class TestPage extends StatelessWidget {
+  const TestPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Swipe Deck"),
+      ),
+      body: Center(
+        child: Container(
+          width: 600,
+          child: Center(
+            child: SwipeDeck(
+              startIndex: 3,
+              emptyIndicator: Container(
+                child: Center(
+                  child: Text("Nothing Here"),
+                ),
+              ),
+              cardSpreadInDegrees: 5, // Change the Spread of Background Cards
+              onSwipeLeft: (){
+                print("USER SWIPED LEFT -> GOING TO NEXT WIDGET");
+              },
+              onSwipeRight: (){
+                print("USER SWIPED RIGHT -> GOING TO PREVIOUS WIDGET");
+              },
+              onChange: (index){
+                print(IMAGES[index]);
+              },
+              widgets: IMAGES
+                  .map((e) => GestureDetector(
+                onTap: () {
+                  print(e);
+                },
+                child: ClipRRect(
+                    borderRadius: borderRadius,
+                    child: Image.asset(
+                      "assets/images/$e.jpg",
+                      fit: BoxFit.cover,
+                    )),
+              ))
+                  .toList(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -83,8 +169,8 @@ class _MyHomePageState extends State<MyHomePage> {
             context: context,
             items: [
              Container(
-               height: 500,
-               width: 350,
+               height: 600,
+               width: 600,
                color: Colors.greenAccent,
              ),
              Container(
